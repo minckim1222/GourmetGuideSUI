9a2394a3304ea277153e55b2e1ff0d007042cb9f
Min Kim <min.c.kim@gmail.com>
2024-02-04 17:11:28 -0600

    Initial Commit

---
+++ GourmetGuideSUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GourmetGuideSUI
+//
+//  Created by Min Kim on 2/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GourmetGuideSUI/GourmetGuideSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GourmetGuideSUIApp.swift
+//  GourmetGuideSUI
+//
+//  Created by Min Kim on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GourmetGuideSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
